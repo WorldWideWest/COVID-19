cc037ef8bbc7b0b135951f5fdee284e5cffea6ee
WorldWideWest <dzeno.dzafic@windowslive.com>
2021-01-03 17:09:20 +0100

    Logging message

--- scripts/parser.py
+++ scripts/parser.py
@@ -33,12 +33,13 @@ if os.path.isfile("../dataSet/rawData/owid-covid-data.xlsx"):
         
 
 driver.quit()
-#rawData = pd.read_excel(os.path.join("../dataSet/rawData/", "mbih.xlsx"), engine='openpyxl')
-#rawData = rawData[rawData['location'] == 'Bosnia and Herzegovina']
-#rawData = rawData.dropna(axis = 1)
-#rawData = rawData.drop(
-#    ['iso_code', 'continent', 'location', 'total_cases_per_million', 'new_cases_per_million','aged_70_older','gdp_per_capita', 'extreme_poverty', 'cardiovasc_death_rate','diabetes_prevalence', 'female_smokers', 'male_smokers', 'handwashing_facilities', 'hospital_beds_per_thousand', 'life_expectancy', 'human_development_index'], axis = 1)
+rawData = pd.read_excel(os.path.join("../dataSet/rawData/", "mbih.xlsx"), engine='openpyxl')
+rawData = rawData[rawData['location'] == 'Bosnia and Herzegovina']
+rawData = rawData.dropna(axis = 1)
+rawData = rawData.drop(
+    ['iso_code', 'continent', 'location', 'total_cases_per_million', 'new_cases_per_million','aged_70_older','gdp_per_capita', 'extreme_poverty', 'cardiovasc_death_rate','diabetes_prevalence', 'female_smokers', 'male_smokers', 'handwashing_facilities', 'hospital_beds_per_thousand', 'life_expectancy', 'human_development_index'], axis = 1)
 
-#rawData.to_excel(os.path.join("../dataSet/rawData/", "mbih.xlsx"))
+rawData.to_excel(os.path.join("../dataSet/rawData/", "mbih.xlsx"))
 
+print("Task completed successfully, file is parsed and it's available at: ", os.path.join("../dataSet/rawData/", "mbih.xlsx"))
 

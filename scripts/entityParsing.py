4fd106ad0b3bb19d7f33d0cf240a31fc3a4a8fe5
WorldWideWest <dzeno.dzafic@windowslive.com>
2021-01-09 13:21:43 +0100

    Parsing entity dataFrames

--- scripts/entityParsing.py
+++ scripts/entityParsing.py
@@ -21,13 +21,21 @@ def GetDay(dataFrame, column, newColumn, columnIndex):
     return data 
 
 
+def Merge(glob, arrays):
+    dataFrame = glob[['date']]
+    for array in arrays:
+        dataFrame = pd.merge(left = dataFrame, left_on = 'date', how = 'left',
+                            right = array, right_on = array.columns[1]).drop("Datum", axis = 1)
+
+    return dataFrame
+
 ## Importing Files ##
 
 
-glob = pd.read_excel(os.path.join("../dataSet/rawData", "mbih.xlsx", engine = "openpyxl")
-fbih = pd.read_excel(os.path.join("../dataSet/rawData", "fbih.xlsx", engine = "openpyxl")
-rs = pd.read_excel(os.path.join("../dataSet/rawData", "rs.xlsx", engine = "openpyxl")
-bd = pd.read_excel(os.path.join("../dataSet/rawData", "bd.xlsx", engine = "openpyxl")
+glob = pd.read_excel(os.path.join("../dataSet/rawData", "mbih.xlsx"), engine = "openpyxl")
+fbih = pd.read_excel(os.path.join("../dataSet/rawData", "fbih.xlsx"), engine = "openpyxl")
+rs = pd.read_excel(os.path.join("../dataSet/rawData", "rs.xlsx"), engine = "openpyxl")
+bd = pd.read_excel(os.path.join("../dataSet/rawData", "bd.xlsx"), engine = "openpyxl")
 
 
 ## FBIH ##
@@ -36,11 +44,15 @@ fbihTest = GetDay(fbih, "Broj testiranih", "Testirani", 2)
 fbihDied = GetDay(fbih, "Broj smrtnih slučajeva", "Smrtni sl.", 3)
 fbihRec = GetDay(fbih, "Broj oporavljenih osoba", "Oporavljeni", 4)
 
+fbih = Merge(glob, [fbihSl, fbihTest, fbihDied, fbihRec])
+
 ## RS ##
 rsSl = GetDay(rs, "Potvrđeni slučajevi", "Slučajevi", 1)
 rsTest = GetDay(rs, "Broj testiranih", "Testirani", 2)
 rsDied = GetDay(rs, "Broj smrtnih slučajeva", "Smrtni sl.", 3)
 rsRec = GetDay(rs, "Broj oporavljenih osoba", "Oporavljeni", 4)
+
+rs = Merge(glob, [rsSl, rsTest, rsDied, rsRec])
 
 ## BD ##
 bdSl = GetDay(bd, "Potvrđeni slučajevi", "Slučajevi", 1)
@@ -48,4 +60,8 @@ bdTest = GetDay(bd, "Broj testiranih", "Testirani", 2)
 bdDied = GetDay(bd, "Broj smrtnih slučajeva", "Smrtni sl.", 3)
 bdRec = GetDay(bd, "Broj oporavljenih osoba", "Oporavljeni", 4)
 
+bd = Merge(glob, [bdSl, bdTest, bdDied, bdRec])
 
+print(fbih)
+print(rs)
+print(bd)

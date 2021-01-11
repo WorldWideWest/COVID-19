1ee61ef02a95a1c79fe1f7637e21aa282fb78b7c
WorldWideWest <dzeno.dzafic@windowslive.com>
2021-01-11 18:18:50 +0100

    Pushing to heroku

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 mkdir -p ~/.streamlit/
-
-
-echo "\
+echo “\
+[general]\n\
+email = \”dzeno.dzafic@windowslive.com\”\n\
+“ > ~/.streamlit/credentials.toml
+echo “\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
+“ > ~/.streamlit/config.toml

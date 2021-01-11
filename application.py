855737379fffc3d2dda08bbf6b2af4fd5b13dfb4
WorldWideWest <dzeno.dzafic@windowslive.com>
2021-01-11 19:11:01 +0100

    Pushing to heroku

--- application.py
+++ application.py
@@ -2,8 +2,6 @@ import streamlit as st
 import os
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from datetime import datetime as dt
 import cufflinks as cf
 
